b5342542d81f0a8d2872fa876ab250606a5d8d8b
Duraaan <sea.duranc@gmail.com>
2025-10-21 00:42:12 -0300

    chore: setup inicial

---
+++ src/main/java/cl/sdc/iam/IamServiceApiApplication.java
@@ -0,0 +1,13 @@
+package cl.sdc.iam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IamServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/sdc/iam/IamServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.sdc.iam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
