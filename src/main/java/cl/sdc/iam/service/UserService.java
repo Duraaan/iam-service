36f0c5844f2209c56c12c01dc0558d5021eb7104
Duraaan <sea.duranc@gmail.com>
2025-10-23 20:36:01 -0300

    feat: agregar configuración de seguridad y servicio de usuarios

---
+++ src/main/java/cl/sdc/iam/service/UserService.java
@@ -0,0 +1,24 @@
+package cl.sdc.iam.service;
+
+import cl.sdc.iam.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service encargado de la logica de los usuarios que implementa UserDetailsService.
+ */
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado con email: " + username));
+    }
+}
